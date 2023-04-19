944fd4adabecc2bf55dbd42144289f0a11f853b4
yazoo1220 <podman1220@hotmail.com>
2023-04-19 22:47:49 +0300

    Changed uploading audio

--- main.py
+++ main.py
@@ -8,21 +8,16 @@ media = st.selectbox('media',['YouTube','Audio upload'])
 if media == 'YouTube':
   video_url = st.text_input(label='YouTube url',value='https://youtu.be/7EnmlzbocEU')
   st.video(video_url)
+  file = YouTube(video_url).streams.filter(only_audio=True).first().download(filename="audio.mp4")
 else:
-  audio_url = st.file_uploader(label='Upload Audio')
-
-
-
-
-
-file = YouTube(video_url).streams.filter(only_audio=True).first().download(filename="audio.mp4")
+  file = st.file_uploader(label='Upload Audio')
 
 whisper_model = whisper.load_model("base")
 
 load = st.button('Load')
 
-if load:
-  with st.spinner('loading...'):
+if file and load:
+  with st.spinner('loading... This might take minutes'):
       transcription = whisper_model.transcribe(file)
       # print as DataFrame
       df = pd.DataFrame(transcription['segments'], columns=['start', 'end', 'text'])
