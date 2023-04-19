2f748e142494110be9e90c9f9c374d0d86f62901
yazoo1220 <podman1220@hotmail.com>
2023-04-19 11:14:25 +0300

    changed ffmpeg

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pytube import YouTube
 import streamlit as st
 import whisper
 import pandas as pd
-import ffmpeg-python
+import ffmpeg
 
 # media = st.selectbox('media',['YouTube','Audio upload'])
 # if media == 'Youtube':
